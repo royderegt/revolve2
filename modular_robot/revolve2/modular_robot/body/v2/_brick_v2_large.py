9c54ac5d752550850514072ad9fc9a885e10454d
Roy de Regt <royderegt@hotmail.com>
2024-10-17 17:29:28 +0200

    Provide option to make brick's offsets different for front and side

--- modular_robot/revolve2/modular_robot/body/v2/_brick_v2_large.py
+++ modular_robot/revolve2/modular_robot/body/v2/_brick_v2_large.py
@@ -13,10 +13,12 @@ class BrickV2Large(Brick):
 
         :param rotation: The modules' rotation.
         """
+        bone_length = 0.2
         super().__init__(
             rotation=rotation,
-            bounding_box=Vector3([0.15, 0.075, 0.075]),
-            mass=(42.65 + (0.15-0.075) * 0.44531428571) / 1000, # Might need to update later
-            child_offset=offset / 2.0 if offset else 0.15 / 2.0,
+            bounding_box=Vector3([bone_length, 0.075, 0.075]),
+            mass=(42.65 + (bone_length-0.075) * 0.44531428571) / 1000, # Might need to update later
+            front_offset= bone_length / 2.0,
+            side_offset= 0.075 / 2.0,
             sensors=[],
         )
